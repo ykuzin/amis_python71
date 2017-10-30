5f55d70c527203fea1bb2232421695d493d79422
ArtemKozak <artem.kozak2013@gmail.com>
2017-10-30 18:46:10 +0200

    Homework_4
    Тема домашнього завдання: "5. Послідовнісні типи даних"

--- km71/Kozak_Artem/5/task4.py
+++ km71/Kozak_Artem/5/task4.py
@@ -1,1 +1,16 @@
-input()
+list1 = []
+list2 = []
+kegls = int(input(' The number of kegls: '))
+balls = int(input(' The number of balls: '))
+for i in range(kegls+1):
+	list1.append(i)
+for _ in range(balls):
+	balls_from = int(input(' From: '))
+	balls_to = int(input(' To: '))
+	for i in range(balls_from, balls_to+1):
+		list2.append(i)
+for i in list1:
+	if i in list2:
+		print('.', end = '')
+	if i not in list2:
+		print('|', end = '')
